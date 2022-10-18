18b53423ef4b1ef5d00682fd749c3f8c580d8520
joseong9 <jsg8825@gmail.com>
2022-10-18 15:15:34 +0900

    mynodejs

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");//설치된 express 사용 선언
 const app = express();//express 실행 app 변수에 대입
 const router = require("./router/router.js");
 const router1 = require("./router/graderouter.js")
+const router2 = require("./router/joinrouter.js")
 const bodyparser = require("body-parser");
 
 app.use(bodyparser.urlencoded({extended:false}));//post 방식일 때 body 영역을 분석해주는 미들웨어
 
-app.use(router1);
+app.use(router2);
 
 app.listen(3000);
